06f54d5dd074014e53f0461915fe0310c18bf57a
Marcela <mmskrbin@gmail.com>
2024-01-26 10:20:41 +0100

    frontend

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const currentYear = document.getElementById("year");
 
 const getCurrentYear = async () => {
   const response = await fetch("https://whatyearisit-eta.vercel.app/year");
+  //   const response = await fetch("http://localhost:3000/year");
   const { year } = await response.json();
   console.log(year);
   currentYear.textContent = year;
